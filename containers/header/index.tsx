341150cc89bf6ff5f621bd278f8f33ab33684e0b
Hilal <hilal.guvenc@segmentify.com>
2020-08-23 23:44:13 +0300

    added css

--- containers/header/index.tsx
+++ containers/header/index.tsx
@@ -1,13 +1,15 @@
 import React, { FunctionComponent } from "react";
 
 const Header: FunctionComponent = () => {
-  return <div id="header-container">
-    <div className="top">
-      <a>twitter</a>
-      <a>github</a>
-      <a>this site is open source</a>
+  return (
+    <div id="header-container">
+      <div className="top">
+        <a>twitter</a>
+        <a>github</a>
+        <a>this site is open source</a>
+      </div>
+      <header>Blog</header>
     </div>
-    <header>Blog</header>
-    </div>;
+  );
 };
 export { Header };
