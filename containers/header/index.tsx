a52e348767ee1d839ad937d53ce06ca6f5390a8d
Hilal <hilal.guvenc@segmentify.com>
2020-09-06 14:41:51 +0300

    mobile view styles have been edited

--- containers/header/index.tsx
+++ containers/header/index.tsx
@@ -4,8 +4,12 @@ const Header: FunctionComponent = () => {
   return (
     <div id="header-container">
       <div className="top">
-        <a className="social-link-left" href="https://twitter.com/">twitter</a>
-        <a className="social-link-left" href="https://github.com/">github</a>
+        <a className="social-link-left" href="https://twitter.com/">
+          twitter
+        </a>
+        <a className="social-link-left" href="https://github.com/">
+          github
+        </a>
         <a className="social-link-right">this site is open source</a>
       </div>
       <header id="main-header">Blog</header>
--- containers/post-list/index.tsx
+++ containers/post-list/index.tsx
@@ -12,7 +12,7 @@ const PostList: FunctionComponent<Props> = ({ posts }) => {
         posts.map((post, index) => (
           <article key={index} id="post-article">
             <header>
-              <h3> 
+              <h3>
                 <a rel="bookmark" href={`/post/${post.slug}`}>
                   {post.title}
                 </a>
